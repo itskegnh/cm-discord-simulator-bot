260752bde717aa6526700d5b43eb20297bde24ef
William Jackson <itskegnh@gmail.com>
2023-07-05 10:27:43 +1000

    test guilds

--- issue_stocks.py
+++ issue_stocks.py
@@ -2,6 +2,6 @@ from mongo import *
 
 # print(millify(103.00))
 
-# col_stocks.update_many({}, { '$set': { 'owners': [], 'sell_offers': [{ 'amount': 200, 'quantity': 2, 'user': MARKET }], 'buy_orders': [] } })
-# col_stocks.update_many({}, { '$set': { 'transactions': [] }})
-# col_users.delete_many({})
+col_stocks.update_many({}, { '$set': { 'owners': [], 'sell_offers': [{ 'amount': 150, 'quantity': 10, 'user': MARKET }], 'buy_orders': [] } })
+col_stocks.update_many({}, { '$set': { 'transactions': [] }})
+col_users.delete_many({})
